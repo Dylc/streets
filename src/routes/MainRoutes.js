b6cdef9ffd02bf309b7e62f2a11712a49b056094
David Y. Cargatser <cargatser@gmail.com>
2022-11-16 07:27:30 +0200

    feat: add RouterProvider with basic setup for HomePage

---
+++ src/routes/MainRoutes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { HomePage } from "pages";
+// import Loadable from "ui-component/Loadable";
+
+const MainRoutes = {
+  path: "/",
+  element: <HomePage />, // <div> Main</div>,
+  // children: [
+  //   {
+  //     path: "/counter",
+  //     element: <Counter />,
+  //   },
+  // ],
+};
+
+export default MainRoutes;
---
+++ src/routes/index.jsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from "react-router-dom";
+
+// routes
+import MainRoutes from "./MainRoutes";
+
+export default createBrowserRouter([MainRoutes]);
