71f28927b23af7a75f1a5a5758134c125ea0d765
David Y. Cargatser <cargatser@gmail.com>
2022-11-16 14:11:08 +0200

    feat: adjust ui for StreetsSelector

--- src/components/StreetsSelector.js
+++ src/components/StreetsSelector.js
@@ -4,6 +4,23 @@ import Autocomplete from "@mui/material/Autocomplete";
 import STREETS from "store/streets";
 import ClearIcon from "@mui/icons-material/Clear";
 
+import { styled, lighten, darken } from "@mui/system";
+
+const GroupHeader = styled("div")(({ theme }) => ({
+  position: "sticky",
+  top: "-8px",
+  padding: "4px 10px",
+  // color: theme.palette.primary.main,
+  // backgroundColor:
+  //   theme.palette.mode === "light"
+  //     ? lighten(theme.palette.primary.light, 0.85)
+  //     : darken(theme.palette.primary.main, 0.8),
+}));
+
+const GroupItems = styled("ul")({
+  padding: 0,
+});
+
 export default function StreetsSelector({ width, height }) {
   return (
     <Autocomplete
@@ -13,7 +30,15 @@ export default function StreetsSelector({ width, height }) {
       getOptionLabel={(option) => option.title.english}
       sx={{ width }}
       clearIcon={<ClearIcon fontSize="big" />}
-      renderInput={(params) => <TextField {...params} label="Street" />}
+      renderInput={(params) => (
+        <TextField {...params} variant="filled" label="Street" />
+      )}
+      // renderGroup={(params) => (
+      //   <li>
+      //     <GroupHeader>{params.group}</GroupHeader>
+      //     <GroupItems>{params.children}</GroupItems>
+      //   </li>
+      // )}
     />
   );
 }
