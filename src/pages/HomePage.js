1a40d5eb915a8c2d3b365401bc8a3635179b6a6c
David Y. Cargatser <cargatser@gmail.com>
2022-11-16 09:43:37 +0200

    feat: add home image to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,16 @@
 // import { Outlet } from "react-router-dom";
 
+import { styled } from "@mui/material/styles";
+
+const HomeImageBackground = styled("div")`
+  height: 100%;
+  width: 100%;
+  background: url(/modiin_view.jpeg) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+`;
 export default function HomePage() {
-  return <div> HomePage </div>;
+  return <HomeImageBackground />;
 }
