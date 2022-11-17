a59ddaf892c00c3a9deae1334b0827a408aced95
David Y. Cargatser <cargatser@gmail.com>
2022-11-17 07:08:23 +0200

    fix: adjust street selector for new street convention

--- src/components/StreetsSelector.js
+++ src/components/StreetsSelector.js
@@ -4,41 +4,23 @@ import Autocomplete from "@mui/material/Autocomplete";
 import STREETS from "store/streets";
 import ClearIcon from "@mui/icons-material/Clear";
 
-import { styled, lighten, darken } from "@mui/system";
-
-const GroupHeader = styled("div")(({ theme }) => ({
-  position: "sticky",
-  top: "-8px",
-  padding: "4px 10px",
-  // color: theme.palette.primary.main,
-  // backgroundColor:
-  //   theme.palette.mode === "light"
-  //     ? lighten(theme.palette.primary.light, 0.85)
-  //     : darken(theme.palette.primary.main, 0.8),
-}));
-
-const GroupItems = styled("ul")({
-  padding: 0,
-});
-
-export default function StreetsSelector({ width, height }) {
+export default function StreetsSelector({ width }) {
   return (
     <Autocomplete
       id="grouped-demo"
       options={STREETS.sort((a, b) => a.neighborhood - b.neighborhood)}
       groupBy={(option) => option.neighborhood}
-      getOptionLabel={(option) => option.title.english}
+      getOptionLabel={(option) => option.name.english}
       sx={{ width }}
       clearIcon={<ClearIcon fontSize="big" />}
       renderInput={(params) => (
-        <TextField {...params} variant="filled" label="Street" />
+        <TextField
+          {...params}
+          variant="filled"
+          label="Select street here"
+          style={{ height: "100%" }}
+        />
       )}
-      // renderGroup={(params) => (
-      //   <li>
-      //     <GroupHeader>{params.group}</GroupHeader>
-      //     <GroupItems>{params.children}</GroupItems>
-      //   </li>
-      // )}
     />
   );
 }
